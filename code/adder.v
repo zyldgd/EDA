d2a7f58cf79048203192ac9ee6de4da5be49afae
zing <1141822522@qq.com>
2018-08-27 15:03:44 +0800

    Update adder.v

--- code/adder.v
+++ code/adder.v
@@ -1,19 +1,30 @@
-module adder(
+/**************** 普通8位加法器 ****************/
+
+module adder1(
+
+input       [7:0]      A,
+input       [7:0]      B,
+output reg  [8:0]      sum,
+
+assign sum[8:0] = {1'd0, A} + {1'd0, B};
+
+endmodule
+
+
+/*************** 8位流水线加法器 **************/
+module adder2(
 input          clk,
 input          cin,
 input  [7:0]   A,
 input  [7:0]   B,
-
-output reg[7:0]   sum,
-output reg        cout
+output reg [7:0]   sum,
+output reg         cout
 );
   
-
 reg            cout1; //插入的寄存器
 reg   [3:0]    sum1; //插入的寄存器
 reg   [3:0]    A_reg;
 reg   [3:0]    B_reg;//插入的寄存器
-
 
 //第一级流水
 always @(posedge clk) begin 
@@ -21,7 +32,6 @@ always @(posedge clk) begin
     A_reg <= A[7:4];
     B_reg <= B[7:4];
 end
-
 
 //第二级流水
 always @(posedge clk) begin
